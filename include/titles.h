2829349dc58c82aef7e1dc09083774253cb05d7c
Willem Elbers <willem.elbers@gmail.com>
2020-07-27 10:34:30 +0100

    Updated license

--- include/titles.h
+++ include/titles.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * This file is part of DEXM.
+ * This file is part of Mitos.
  * Copyright (c) 2020 Willem Elbers (whe@willemelbers.com)
  *
  * This program is free software: you can redistribute it and/or modify
