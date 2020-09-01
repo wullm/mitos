2993ea030109287ec999b5b51c3533e39a9ffb12
Willem Elbers <willem.elbers@gmail.com>
2020-09-01 08:16:39 +0100

    Improved reference grid structure & export profiles

--- analyse/src/mitos_profiles.c
+++ analyse/src/mitos_profiles.c
@@ -26,6 +26,12 @@
 
 #include "../../include/mitos.h"
 
+/* Used for linked list */
+struct id_link {
+    int id;
+    struct id_link *next;
+};
+
 int main(int argc, char *argv[]) {
     if (argc == 1) {
         printf("No parameter file specified.\n");
@@ -62,8 +68,8 @@ int main(int argc, char *argv[]) {
     message(rank, "Reading halos from '%s'.\n", pars.HaloInputFilename);
     hid_t h_halo_file = openFile_MPI(MPI_COMM_WORLD, pars.HaloInputFilename);
 
-    /* Open the X-coordinates dataset */
-    hid_t h_halo_dat = H5Dopen2(h_halo_file, "Xc", H5P_DEFAULT);
+    /* Open the halo masses dataset */
+    hid_t h_halo_dat = H5Dopen2(h_halo_file, "Mass_tot", H5P_DEFAULT);
 
     /* Open the dataspace and fetch the grid dimensions */
     hid_t h_halo_space = H5Dget_space(h_halo_dat);
@@ -71,15 +77,17 @@ int main(int argc, char *argv[]) {
     H5Sget_simple_extent_dims(h_halo_space, &halo_num, NULL);
     H5Sclose(h_halo_space);
 
-    /* Close the dataset */
-    H5Dclose(h_halo_dat);
-
-    /* Allocate memory for the halo centre coordinates */
+    /* Allocate memory for the halo masses and coordinates */
+    double *halo_M = malloc(halo_num * sizeof(double));
     double *halo_x = malloc(halo_num * sizeof(double));
     double *halo_y = malloc(halo_num * sizeof(double));
     double *halo_z = malloc(halo_num * sizeof(double));
 
     message(rank, "We have %lld halos\n", halo_num);
+
+    /* Read out the masses */
+    H5Dread(h_halo_dat, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, halo_M);
+    H5Dclose(h_halo_dat);
 
     /* Read out the X-coordinates */
     h_halo_dat = H5Dopen2(h_halo_file, "Xc", H5P_DEFAULT);
@@ -186,51 +194,47 @@ int main(int argc, char *argv[]) {
 
 
 
-
-
-
     /* The size of the linked list grid */
     const int N = 256; //pars.GridSize;
 
-    /* Maximum number of halos per cell */
-    int max_halos_per_cell = 100;
-
     /* Allocate grids */
-    int *box = malloc(N * N * N * max_halos_per_cell * sizeof(int));
-    int *halos = calloc(N * N * N, sizeof(int));
-
+    struct id_link *halo_links = malloc(halo_num * sizeof(struct id_link));
+    struct id_link **halo_grid = malloc(N * N * N * sizeof(struct id_link*));
+    int *halos_in_cell = calloc(N * N * N, sizeof(int));
 
     /* Insert the halos */
     for (int i=0; i<halo_num; i++) {
+        /* Transform physical coordinates to grid coordinates */
         double X = halo_x[i] / (boxlen[0]/N);
         double Y = halo_y[i] / (boxlen[1]/N);
         double Z = halo_z[i] / (boxlen[2]/N);
 
+        /* Integer coordinas of the halo */
         int iX = (int) floor(X);
         int iY = (int) floor(Y);
         int iZ = (int) floor(Z);
 
+        /* ID of the grid cell */
         int id = row_major(iX, iY, iZ, N);
 
-        // printf("%d %d %d\n", iX, iY, iZ);
-
-        /* Insert the halo id */
-        box[id*max_halos_per_cell + halos[id]] = i;
-        halos[id]++;
-        if (halos[id] > max_halos_per_cell) {
-            printf("Error: grid too small.\n");
-            exit(1);
+        /* If it is the first halo in this cell */
+        if (halos_in_cell[id] == 0) {
+            /* Insert in the cell without a link */
+            halo_links[i].id = i;
+            halo_links[i].next = NULL;
+            halo_grid[id] = &halo_links[i];
+        } else {
+            /* Otherwise, insert in front of the current linked list */
+            halo_links[i].id = i;
+            halo_links[i].next = halo_grid[id];
+            halo_grid[id] = &halo_links[i];
         }
+
+        /* Increment the halos in cell counter */
+        halos_in_cell[id]++;
     }
 
-    printf("Hiero\n");
-
-
-
-
-
-
-
+    printf("Linked list grid created\n");
 
     double total_mass = 0; //for this particle type
 
@@ -313,39 +317,7 @@ int main(int argc, char *argv[]) {
         /* Close the dataset */
         H5Dclose(h_dat);
 
-
-        // /* Open the velocities dataset */
-        // h_dat = H5Dopen(h_grp, "Velocities", H5P_DEFAULT);
-        //
-        // /* Find the dataspace (in the file) */
-        // h_space = H5Dget_space (h_dat);
-        //
-        // /* Select the hyperslab */
-        // status = H5Sselect_hyperslab(h_space, H5S_SELECT_SET, start,
-        //                              NULL, slab_dims, NULL);
-        // assert(status >= 0);
-        //
-        // /* Create a memory space */
-        // h_mems = H5Screate_simple(2, slab_dims, NULL);
-        //
-        // /* Create the data array */
-        // double velocities_data[slab_size][3];
-        //
-        // status = H5Dread(h_dat, H5T_NATIVE_DOUBLE, h_mems, h_space, H5P_DEFAULT,
-        //                  velocities_data);
-        //
-        // /* Close the memory space */
-        // H5Sclose(h_mems);
-        //
-        // /* Close the data and memory spaces */
-        // H5Sclose(h_space);
-        //
-        // /* Close the dataset */
-        // H5Dclose(h_dat);
-
-        double grid_cell_vol = boxlen[0]*boxlen[1]*boxlen[2] / (N*N*N);
-
-        /* Assign the particles to the grid with CIC */
+        /* Assign the particles to the halo profiles */
         for (int l=0; l<slab_size; l++) {
             double X = data[l][0] / (boxlen[0]/N);
             double Y = data[l][1] / (boxlen[1]/N);
@@ -355,29 +327,26 @@ int main(int argc, char *argv[]) {
             int iY = (int) floor(Y);
             int iZ = (int) floor(Z);
 
-
-            // double V_X = velocities_data[l][0];
-            // double V_Y = velocities_data[l][1];
-            // double V_Z = velocities_data[l][2];
-            //
-            // /* Unused variables */
-            // (void) V_X;
-            // (void) V_Y;
-            // (void) V_Z;
-
             double M = mass_data[l];
             total_mass += M;
 
             /* Find halos that are close */
-            for (int i=-1; i<=1; i++) {
-                for (int j=-1; j<=1; j++) {
-                    for (int k=-1; k<=1; k++) {
-                        int halos_in_cell = halos[row_major(iX + i, iY + j, iZ + k, N)];
+            for (int dx=-1; dx<=1; dx++) {
+                for (int dy=-1; dy<=1; dy++) {
+                    for (int dz=-1; dz<=1; dz++) {
+                        /* Grid cell ID */
+                        int grid_id = row_major(iX + dx, iY + dy, iZ + dz, N);
+                        /* Number of halos in this cell */
+                        int h_max = halos_in_cell[grid_id];
 
-                        for (int h=0; h<halos_in_cell; h++) {
+                        /* Next halo */
+                        struct id_link *next = halo_grid[grid_id];
 
+                        for (int h=0; h<h_max; h++) {
                             /* Halo id */
-                            int h_id = box[row_major(iX + i, iY + j, iZ + k, N) * max_halos_per_cell + h];
+                            int h_id = next->id;
+                            /* Next halo */
+                            next = next->next;
 
                             /* Determine distance to halo centre */
                             double h_x = halo_x[h_id];
@@ -387,9 +356,8 @@ int main(int argc, char *argv[]) {
                             double r2 = (h_x - X)*(h_x - X) + (h_y - Y)*(h_y - Y) + (h_z - Z)*(h_z - Z);
 
                             if (r2 < r2_max) {
-                                // printf("%f (%f, %f) (%f, %f) (%f, %f) %d %d\n", r2, X, h_x, Y, h_y, Z, h_z, h, h_id);
                                 /* Put the particle in a bin */
-                                int bin = floor(r2 / r2_max * num_bins);
+                                int bin = floor(sqrt(r2 / r2_max) * num_bins);
                                 profiles[h_id * num_bins + bin] += M;
                             }
                         }
@@ -397,12 +365,6 @@ int main(int argc, char *argv[]) {
                     }
                 }
             }
-
-            /* Add to all the appropriate profiles */
-
-
-
-
         }
 
         printf("(%03d,%03d) Read %ld particles\n", rank, k, slab_size);
@@ -447,21 +409,6 @@ int main(int argc, char *argv[]) {
 
         message(rank, "Average density %f\n", avg_density);
 
-        // if (strcmp(tp.Identifier, "ncdm") == 0) {
-        //     avg_density = 0.179075;
-        //     printf("Reset avg_density to %f\n", avg_density);
-        // }
-
-        /* Turn the density field into an overdensity field */
-        // for (int x=0; x<N; x++) {
-        //     for (int y=0; y<N; y++) {
-        //         for (int z=0; z<N; z++) {
-        //             int id = row_major(x, y, z, N);
-        //             box[id] = (box[id] - avg_density)/avg_density;
-        //         }
-        //     }
-        // }
-
         /* Find a particle type with a matching ExportName */
         struct particle_type *tp;
         char found = 0;
@@ -475,21 +422,82 @@ int main(int argc, char *argv[]) {
             }
         }
 
-        // char box_fname[40];
-        // if (!found) {
-        //     sprintf(box_fname, "density_%s.hdf5", pars.ImportName);
-        // } else {
-        //     sprintf(box_fname, "density_%s.hdf5", tp->Identifier);
-        // }
-        // writeFieldFile(box, N, boxlen[0], box_fname);
-        // message(rank, "Density grid exported to %s.\n", box_fname);
+        /* Export the halo profiles */
+        char prof_fname[40];
+        if (!found) {
+            sprintf(prof_fname, "profiles_%s.hdf5", pars.ImportName);
+        } else {
+            sprintf(prof_fname, "profiles_%s.hdf5", tp->Identifier);
+        }
+
+        /* Create the profiles file */
+        hid_t out_file = createFile(prof_fname);
+
+        /* Create the Halos group */
+        hid_t h_exgrp = H5Gcreate(out_file, "/Halos", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+
+        /* Create dataspace for the profiles */
+        const hsize_t crank = 2;
+        const hsize_t cdims[2] = {halo_num, num_bins}; //3D space
+        hid_t h_cspace = H5Screate_simple(crank, cdims, NULL);
+
+        /* Create the dataset for the profiles */
+        hid_t h_data = H5Dcreate(h_exgrp, "Profiles", H5T_NATIVE_DOUBLE, h_cspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+
+        /* Write the data */
+        H5Dwrite(h_data, H5T_NATIVE_DOUBLE, h_cspace, h_cspace, H5P_DEFAULT, profiles);
+
+        /* Close the dataset & corresponding dataspace */
+        H5Dclose(h_data);
+        H5Sclose(h_cspace);
+
+        /* Create dataspace for the masses */
+        const hsize_t srank = 1;
+        const hsize_t sdims[1] = {halo_num};
+        hid_t h_sspace = H5Screate_simple(srank, sdims, NULL);
+
+        /* Create the dataset for the masses */
+        h_data = H5Dcreate(h_exgrp, "Mass_tot", H5T_NATIVE_DOUBLE, h_sspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+
+        /* Write the data */
+        H5Dwrite(h_data, H5T_NATIVE_DOUBLE, h_sspace, h_sspace, H5P_DEFAULT, halo_M);
+
+        /* Close the dataset */
+        H5Dclose(h_data);
+
+        /* Export X-coordinates */
+        h_data = H5Dcreate(h_exgrp, "Xc", H5T_NATIVE_DOUBLE, h_sspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+        H5Dwrite(h_data, H5T_NATIVE_DOUBLE, h_sspace, h_sspace, H5P_DEFAULT, halo_x);
+        H5Dclose(h_data);
+
+        /* Export X-coordinates */
+        h_data = H5Dcreate(h_exgrp, "Yc", H5T_NATIVE_DOUBLE, h_sspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+        H5Dwrite(h_data, H5T_NATIVE_DOUBLE, h_sspace, h_sspace, H5P_DEFAULT, halo_y);
+        H5Dclose(h_data);
+
+        /* Export Z-coordinates */
+        h_data = H5Dcreate(h_exgrp, "Zc", H5T_NATIVE_DOUBLE, h_sspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+        H5Dwrite(h_data, H5T_NATIVE_DOUBLE, h_sspace, h_sspace, H5P_DEFAULT, halo_z);
+        H5Dclose(h_data);
+
+        /* Close the scalar dataspace */
+        H5Sclose(h_sspace);
+
+        /* Close the Halos group */
+        H5Gclose(h_exgrp);
+
+        /* Close the profiles file */
+        H5Fclose(out_file);
+
     }
 
     /* Free the linked list grid */
-    free(box);
-    free(halos);
+    free(halo_links);
+    free(halo_grid);
+    free(halos_in_cell);
 
     /* Free memory for the halo stats */
+    free(halo_M);
     free(halo_x);
     free(halo_y);
     free(halo_z);
